c6261c54932cd3da6985d1d3197f8a1a14abb758
n-pontes <nuno06.np@gmail.com>
2023-12-07 14:17:41 +0100

    Modify the function playRound

--- rps.js
+++ rps.js
@@ -1,25 +1,29 @@
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === 'paper' && computerSelection === 'rock' 
-    || playerSelection === 'scissors' && computerSelection === 'paper' 
-    || playerSelection === 'rock' && computerSelection === 'scissors') {
-        return alert('player wins');
-    } 
-}
-
-
-
-
 function getComputerChoice() {
-      const randomNumber = Math.floor(Math.random() * 3);
-      
-      switch(randomNumber) {
-      case 0:
-        return "rock";
-      case 1:
-        return "paper";
-      case 2:
-        return "scissors";
-      }
+    const randomNumber = Math.floor(Math.random() * 3);
+    
+    switch(randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    }
 }
 
-console.log(getComputerChoice());
+function playRound (playerSelection, computerSelection) {
+        if (playerSelection === 'rock' && computerSelection === 'rock') {
+            console.log("Its a tie!");
+        } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+            console.log("Paper beats Rock, you lose!");
+        } else {
+            console.log ("Rock beats Scissors, you win!");
+        }
+    }
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+
