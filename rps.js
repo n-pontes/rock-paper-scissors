8923a21a52dceb64710e55c51f20fe8bf9376045
n-pontes <nuno06.np@gmail.com>
2023-12-07 19:14:32 +0100

    Add function playRound, contains the logic

--- rps.js
+++ rps.js
@@ -7,3 +7,26 @@ function getComputerChoice() {
 // Tests the randomizer function, if it prints out Rock, Paper or SCissors
 
 //console.log(getComputerChoice());
+
+/**
+ * This is the round function, allows to update the player score and computer score
+ * Contains the logic for the game
+ * 
+*/
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        playerScore += 1;
+        return `You win!`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore += 1;
+        return `You win!`;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        playerScore += 1;
+        return `You win!`;
+    } else {
+        computerScore += 1;
+        return `You lose!`;
+    }
+}
