f16929abbe38cf9f9e03adbdbc1be44365623827
n-pontes <nuno06.np@gmail.com>
2024-02-06 11:43:43 +0100

    Remove older functions in comments - replace by arrow functions

--- rps.js
+++ rps.js
@@ -1,4 +1,6 @@
-// Global variables for the player and computers score
+/* 
+Global variables for the player and computers score
+*/
 
 let playerScore = 0;
 let computerScore = 0;
@@ -7,13 +9,6 @@ let computerScore = 0;
 This function is responsible for making the computer play
 It randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’.
 */
-
-
-/*function getComputerChoice() {
-    let allChoices = ["rock", "paper", "scissors"];
-    let randomChoice = allChoices[Math.floor(Math.random() * allChoices.length)];
-    return randomChoice;
-}*/
 
 getComputerChoice = () => {
     let allChoices = ["rock", "paper", "scissors"];
@@ -24,14 +19,6 @@ getComputerChoice = () => {
 /**
  * The game keeps on playing until either the player or the computer achieves 5 points
  */
-
-/*function game () {
-    while (playerScore < 5 && computerScore < 5) {
-        const playerSelection = prompt("Do you choose, rock, paper or scissors?");
-        const computerSelection = getComputerChoice();
-        alert(playRound(playerSelection, computerSelection));
-    }
-}*/
 
 game = () => {
     while (playerScore < 5 && computerScore < 5) {
@@ -45,29 +32,6 @@ game = () => {
  * the function playRound grabs two parameters player choice and the random parameter for the computer
  * 
 */
-
-/*function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase(); // function to transform the string into lowercase just so its not case sensitive
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (
-        (playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock')
-    ) {
-        playerScore += 1;
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
-    } else if (
-        (computerSelection === 'scissors' && playerSelection === 'paper') ||
-        (computerSelection === 'rock' && playerSelection === 'scissors') ||
-        (computerSelection === 'paper' && playerSelection === 'rock')
-    ) {
-        computerScore += 1;
-        return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    } else {
-        return "Invalid Input!";
-    }
-}*/
 
 playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection.toLowerCase(); // function to transform the string into lowercase just so its not case sensitive
@@ -92,13 +56,10 @@ playRound = (playerSelection, computerSelection) => {
     }
 };
 
-/*function winGame () {
-    if (playerScore == 5) {
-        return "You won the game!";
-    } else if (computerScore == 5) {
-        return "You lost the game!";
-    }
-}*/
+/*
+This function verifies if the player score is higher than the computer
+If that is the case player wins the game otherwise he loses
+*/
 
 winGame = () => {
     if (playerScore == 5) {
